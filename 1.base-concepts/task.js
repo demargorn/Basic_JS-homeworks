e72f29f396b5cfac666b663c3f683696f6bf1e53
Михаил Дорошин <demargorn@gmail.com>
2024-04-25 22:15:43 +0300

    git commit -m 'Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+'use strict';
+
+// задание 1
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let x1, x2;
+  let discriminant = (b ** 2) - (4 * a * c);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  if (discriminant < 0) {
+    arr = [];
+  };
+  if (discriminant === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  };
+  if (discriminant > 0) {
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1, x2);
+  };  
+  console.log(arr);
+  return arr;
+};
+
+solveEquation(1, 5, 4); // ok
