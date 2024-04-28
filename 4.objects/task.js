3b16cd305e0ed6b369c6cd0cb75f2fbe01a6ef5f
Михаил Дорошин <demargorn@gmail.com>
2024-04-28 21:25:07 +0300

    Все студенты описаны

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
+'use strict';
+
 function Student(name, gender, age) {
-  
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
+   this.marks = [];
+};
+
+Student.prototype.setSubject = function(subjectName) {
+   this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+   if(this.marks === undefined) { 
+      return false;
+   } else {
+      this.marks.push(...marksToAdd);
+   };
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+   if (this.marks === undefined || this.marks.length === 0) {
+      return 0;
+   };
+   return this.marks.reduce((acc, el) => acc += el / this.marks.length, 0); 
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.exclude = function(reason) {
+   this.excluded = reason;
+   delete this.subject;
+   delete this.marks;
 }
 
-Student.prototype.exclude = function (reason) {
-  
-}
+let student = new Student("Василиса", "женский", 19);
+console.log(student.exclude('прогулы'));
+// expect(student.excluded).toEqual('прогулы');
+console.log(student.addMarks(5, 5, 5));
+   //  expect(student.marks).toBeUndefined();
+   //  expect(student.getAverage()).toEqual(0);
