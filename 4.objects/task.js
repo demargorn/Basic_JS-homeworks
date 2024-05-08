fb3a2774e9362e934c6338d788da7437ca6aeaaf
Михаил Дорошин <demargorn@gmail.com>
2024-05-08 12:55:42 +0300

    Все студенты описаны

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,7 +9,7 @@ function Student(name, gender, age) {
 
 Student.prototype.setSubject = function(subjectName) {
    this.subject = subjectName;
-}
+};
 
 Student.prototype.addMarks = function(...marksToAdd) {
    if(this.marks === undefined) { 
@@ -17,17 +17,17 @@ Student.prototype.addMarks = function(...marksToAdd) {
    } else {
       this.marks.push(...marksToAdd);
    };
-}
+};
 
 Student.prototype.getAverage = function() {
    if (this.marks === undefined || this.marks.length === 0) {
       return 0;
    };
-   return this.marks.reduce((acc, el) => acc += el / this.marks.length, 0); 
-}
+   return this.marks.reduce((acc, el) => acc + el / this.marks.length, 0); 
+};
 
 Student.prototype.exclude = function(reason) {
    this.excluded = reason;
    delete this.subject;
    delete this.marks;
-}
+};
